b81be04f15a24f7de17a2006742e2c357e36381a
Vittorio Lo Mele <vittorio@mrbackslash.it>
2021-11-14 02:00:00 +0100

    clean dir

--- src/main/java/dev/vitto/gipcat/repository/ContactInformationTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.vitto.gipcat.repository;
-
-import dev.vitto.gipcat.model.ContactInformationType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactInformationTypeRepository extends JpaRepository<ContactInformationType, Integer> {
-}
